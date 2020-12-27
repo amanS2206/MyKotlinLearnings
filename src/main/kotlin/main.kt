bf4848e57187c324a0dea436163eda640b961580
Kanishk Bharadwaj <kanishkbharadwaj333@gmail.com>
2020-12-27 18:21:21 +0530

    kotlin number class comment commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -14,4 +14,15 @@ fun main(args: Array<String>) {
 */
 
 
+/*
+Everything is a class in Kotlin
+
+There are 4 types of Number classes in Kotlin:
+  1. Byte - 1 Byte
+  2. Short - 2 Byte
+  3. Int - 4 Byte
+  4. Long - 8 Byte*/
+
+
+
 }
